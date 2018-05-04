5351d8bb02f576c479274d6528bb37a856f5deab
Aaron LalorFitzpatrick <aaron.lalorfitzpatrick2@mail.dcu.ie>
2018-05-04 17:56:41 +0100

    Fixed syntax errors

--- arbitrage.py
+++ arbitrage.py
@@ -35,20 +35,20 @@ class Exchange:
         self.exchangeType = exchangeType
         self.tradingPaused = False
         self.trial = trial
-		
-		self.ccxtObject = getattr(ccxt,self.NAME)()
+        
+        self.ccxtObject = getattr(ccxt,self.NAME)()
         self.ccxtObject.loadMarkets()
         self.ccxtObject.apiKey = exchangeDict['publicKey']
         self.ccxtObject.secret = exchangeDict['privateKey']
         
-		if self.trial == False:
-			balances = self.ccxtObject.fetchBalance()
-			self.availableBalanceBASE = balances['free'][Exchange.BASE_CURRENCY]
-			self.availableBalanceQUOTE = balances['free'][Exchange.QUOTE_CURRENCY]
+        if self.trial == False:
+            balances = self.ccxtObject.fetchBalance()
+            self.availableBalanceBASE = balances['free'][Exchange.BASE_CURRENCY]
+            self.availableBalanceQUOTE = balances['free'][Exchange.QUOTE_CURRENCY]
         else:
-			self.availableBalanceBASE = 10 # 10 BASE coins
-			self.availableBalanceQUOTE = 0.25 # 0.25 QUOTE coins, e.g. BTC
-			
+            self.availableBalanceBASE = 10 # 10 BASE coins
+            self.availableBalanceQUOTE = 0.25 # 0.25 QUOTE coins, e.g. BTC
+            
         self.orders = self.getOrderbook()
         self.profitStats = {}
         
@@ -165,7 +165,7 @@ class Exchange:
                     break
             time.sleep(1)
     
-	
+    
     def trade(self):
         self.checkBalanceBASE()
         self.checkBalanceQUOTE()
@@ -190,18 +190,18 @@ class Exchange:
                     # The deposit has been completed successfully. Proceed.
                     break
             time.sleep(1)
-		balanceDifferences = {
-						'balanceBeforeBASE':[balanceBeforeBASE],
-						'balanceBeforeQUOTE':[balanceBeforeQUOTE],
-						'balanceAfterBASE':[self.checkBalanceBASE()],
-						'balanceAfterQUOTE':[self.checkBalanceQUOTE()],
-						'balanceDifferenceBASE':[(self.availableBalanceBASE - balanceBeforeBASE)],
-						'balanceDifferenceQUOTE':[(self.availableBalanceQUOTE - balanceBeforeQUOTE)],
-						'time':[time.strftime("%d-%m-%Y %H:%M:%S")]
-						}
-		return balanceDifferences
+        balanceDifferences = {
+                        'balanceBeforeBASE':[balanceBeforeBASE],
+                        'balanceBeforeQUOTE':[balanceBeforeQUOTE],
+                        'balanceAfterBASE':[self.checkBalanceBASE()],
+                        'balanceAfterQUOTE':[self.checkBalanceQUOTE()],
+                        'balanceDifferenceBASE':[(self.availableBalanceBASE - balanceBeforeBASE)],
+                        'balanceDifferenceQUOTE':[(self.availableBalanceQUOTE - balanceBeforeQUOTE)],
+                        'time':[time.strftime("%d-%m-%Y %H:%M:%S")]
+                        }
+        return balanceDifferences
     
-	
+    
     def withdraw(self):
         if self.exchangeType == 'BUY':
             currency = Exchange.BASE_CURRENCY
@@ -218,65 +218,65 @@ class Exchange:
         self.checkBalanceQUOTE()
         self.withdraw()
         balanceDifferences = self.waitForDeposit()
-		
-		'''
-		The following code outputs the net profits and balances on the 
-		exchange, either the buyExchange or the sellExchange.
-		'''
-		exchange_output_text = (
-						'Net profits earned for ' + self.NAME + ': \n'
-						+ Exchange.BASE_CURRENCY + 'gained/lost = ' + balanceDifferences['balanceDifferenceBASE'][0] + '\n'
-						+ Exchange.QUOTE_CURRENCY + 'gained/lost = ' + balanceDifferences['balanceDifferenceQUOTE'][0] + '\n'
-						+ 'Time: ' + balanceDifferences['time'][0] + '\n\n\n'
-						)
-		with threading.Lock():
-			print(exchange_output_text)
-			with open('output_'+self.NAME+'.txt','a') as t:
-				t.write(exchange_output_text)
-			with open('balances_'+self.NAME+'.csv','a') as c:
-				pd.DataFrame(balanceDifferences.to_csv(c,header=False)
-		
-		if self.exchangeType == 'BUY':
-			BothExchangesInstance.balanceDifferenceBASE_BUY_EXCHANGE = balanceDifferences['balanceDifferenceBASE'][0]
-			BothExchangesInstance.balanceDifferenceQUOTE_BUY_EXCHANGE = balanceDifferences['balanceAfterQUOTE'][0]
-		else:
-			BothExchangesInstance.balanceDifferenceBASE_SELL_EXCHANGE = balanceDifferences['balanceDifferenceBASE'][0]
-			BothExchangesInstance.balanceDifferenceQUOTE_SELL_EXCHANGE = balanceDifferences['balanceAfterQUOTE'][0]
-				
-	#--------------------------------
+        
+        '''
+        The following code outputs the net profits and balances on the 
+        exchange, either the buyExchange or the sellExchange.
+        '''
+        exchange_output_text = (
+                        'Net profits earned for ' + self.NAME + ': \n'
+                        + Exchange.BASE_CURRENCY + 'gained/lost = ' + balanceDifferences['balanceDifferenceBASE'][0] + '\n'
+                        + Exchange.QUOTE_CURRENCY + 'gained/lost = ' + balanceDifferences['balanceDifferenceQUOTE'][0] + '\n'
+                        + 'Time: ' + balanceDifferences['time'][0] + '\n\n\n'
+                        )
+        with threading.Lock():
+            print(exchange_output_text)
+            with open('output_'+self.NAME+'.txt','a') as t:
+                t.write(exchange_output_text)
+            with open('balances_'+self.NAME+'.csv','a') as c:
+                pd.DataFrame(balanceDifferences).to_csv(c,header=False)
+        
+        if self.exchangeType == 'BUY':
+            BothExchangesInstance.balanceDifferenceBASE_BUY_EXCHANGE = balanceDifferences['balanceDifferenceBASE'][0]
+            BothExchangesInstance.balanceDifferenceQUOTE_BUY_EXCHANGE = balanceDifferences['balanceAfterQUOTE'][0]
+        else:
+            BothExchangesInstance.balanceDifferenceBASE_SELL_EXCHANGE = balanceDifferences['balanceDifferenceBASE'][0]
+            BothExchangesInstance.balanceDifferenceQUOTE_SELL_EXCHANGE = balanceDifferences['balanceAfterQUOTE'][0]
+                
+    #--------------------------------
 
         
-		
+        
 #========================================================================================================
         
 
-		
+        
 class BothExchanges:
 
-	def __init__(self):
-		self.balanceDifferenceBASE_BUY_EXCHANGE = 0
-		self.balanceDifferenceBASE_SELL_EXCHANGE = 0
-		self.balanceDifferenceQUOTE_BUY_EXCHANGE = 0
-		self.balanceDifferenceQUOTE_SELL_EXCHANGE = 0
-		self.profitGainedBASE = 0
-		self.profitGainedQUOTE = 0
-		
-	def getProfitBASE(self):
-		self.profitGainedBASE = (self.balanceDifferenceBASE_BUY_EXCHANGE 
-								+ self.balanceDifferenceBASE_SELL_EXCHANGE)
-		return self.profitGainedBASE
-	
-	def getProfitQUOTE(self):
-		self.profitGainedQUOTE = (self.balanceDifferenceQUOTE_BUY_EXCHANGE 
-								+ self.balanceDifferenceQUOTE_SELL_EXCHANGE)
-		return self.profitGainedQUOTE	
-		
-		
-		
-	
-#========================================================================================================	
-		
-		
+    def __init__(self):
+        self.balanceDifferenceBASE_BUY_EXCHANGE = 0
+        self.balanceDifferenceBASE_SELL_EXCHANGE = 0
+        self.balanceDifferenceQUOTE_BUY_EXCHANGE = 0
+        self.balanceDifferenceQUOTE_SELL_EXCHANGE = 0
+        self.profitGainedBASE = 0
+        self.profitGainedQUOTE = 0
+        
+    def getProfitBASE(self):
+        self.profitGainedBASE = (self.balanceDifferenceBASE_BUY_EXCHANGE 
+                                + self.balanceDifferenceBASE_SELL_EXCHANGE)
+        return self.profitGainedBASE
+    
+    def getProfitQUOTE(self):
+        self.profitGainedQUOTE = (self.balanceDifferenceQUOTE_BUY_EXCHANGE 
+                                + self.balanceDifferenceQUOTE_SELL_EXCHANGE)
+        return self.profitGainedQUOTE    
+        
+        
+        
+    
+#========================================================================================================    
+        
+        
 def buyAndSell(BuyExchange,SellExchange):
     buyThread = threading.Thread(target=BuyExchange.trade(),daemon=True)
     sellThread = threading.Thread(target=SellExchange.trade(),daemon=True)    
@@ -287,138 +287,138 @@ def buyAndSell(BuyExchange,SellExchange):
     sellThread.join()
 
 
-	
+    
 def moveFunds(BuyExchange,SellExchange,BothExchanges):
-    buyExchangeThread = threading.Thread(target=BuyExchange.withdrawAndConfirm,argument=(BothExchanges,)daemon=True)
-    sellExchangeThread = threading.Thread(target=SellExchange.withdrawAndConfirm,argument=BothExchanges,)daemon=True)
+    buyExchangeThread = threading.Thread(target=BuyExchange.withdrawAndConfirm,argument=(BothExchanges,),daemon=True)
+    sellExchangeThread = threading.Thread(target=SellExchange.withdrawAndConfirm,argument=(BothExchanges,),daemon=True)
     
     buyExchangeThread.start()
     sellExchangeThread.start()
     buyExchangeThread.join()
     sellExchangeThread.join()
      
-	'''
-	The following lines of code outputs the NET PROFIT MADE FROM BOTH EXCHANGES
-	'''
-	profits = {
-			'netProfitBASE':[BothExchanges.getProfitBASE()],
-			'netProfitQUOTE':[BothExchanges.getProfitQUOTE()],
-			'time':[time.strftime("%d-%m-%Y %H:%M:%S")]
-			}
-	output_text_both = (
-			'Net profits earned between both exchanges: \n'
-			+ BuyExchange.BASE_CURRENCY + 'gained/lost = ' + profits['netProfitBASE'][0] + '\n'
-			+ BuyExchange.QUOTE_CURRENCY + 'gained/lost = ' + profits['netProfitQUOTE'][0] + '\n'
-			+ 'Time: ' + profits['time'][0] + '\n\n\n'
-			)
-	with threading.Lock():
-		print(output_text_both)
-		with open('output_both.txt','a') as t:
-			t.write(output_text_both)
-		with open('profits.csv','a') as c:
-			pd.DataFrame(profits).to_csv(c,header=False)		
+    '''
+    The following lines of code outputs the NET PROFIT MADE FROM BOTH EXCHANGES
+    '''
+    profits = {
+            'netProfitBASE':[BothExchanges.getProfitBASE()],
+            'netProfitQUOTE':[BothExchanges.getProfitQUOTE()],
+            'time':[time.strftime("%d-%m-%Y %H:%M:%S")]
+            }
+    output_text_both = (
+            'Net profits earned between both exchanges: \n'
+            + BuyExchange.BASE_CURRENCY + 'gained/lost = ' + profits['netProfitBASE'][0] + '\n'
+            + BuyExchange.QUOTE_CURRENCY + 'gained/lost = ' + profits['netProfitQUOTE'][0] + '\n'
+            + 'Time: ' + profits['time'][0] + '\n\n\n'
+            )
+    with threading.Lock():
+        print(output_text_both)
+        with open('output_both.txt','a') as t:
+            t.write(output_text_both)
+        with open('profits.csv','a') as c:
+            pd.DataFrame(profits).to_csv(c,header=False)        
     
     
-	
+    
 #========================================================================================================
 
 
   
 def arbitrage(BuyExchange,SellExchange,BothExchanges,minProfitEuro):
 
-	BuyExchangeOrderbookThread = threading.Thread(target=BuyExchange.getOrderbooksContinuously,daemon=True)
-	SellExchangeOrderbookThread = threading.Thread(target=SellExchange.getOrderbooksContinuously,daemon=True)
-	BuyExchangeOrderbookThread.start()
-	SellExchangeOrderbookThread.start()
-	time.sleep(3)
-	
+    BuyExchangeOrderbookThread = threading.Thread(target=BuyExchange.getOrderbooksContinuously,daemon=True)
+    SellExchangeOrderbookThread = threading.Thread(target=SellExchange.getOrderbooksContinuously,daemon=True)
+    BuyExchangeOrderbookThread.start()
+    SellExchangeOrderbookThread.start()
+    time.sleep(3)
+    
     while True:
         while BuyExchange.tradingPaused == False and SellExchange.tradingPaused == False: 
             profitCalculations = pc.profitabilityCalculator(BuyExchange,SellExchange)
             BuyExchange.profitStats = profitCalculations
             SellExchange.profitstats = profitCalculations
-			
+            
             if profitCalculations['netProfitEuro'][0] > minProfitEuro:
                 BuyExchange.tradingPaused = True
                 SellExchange.tradingPaused = True
-				
+                
                 buyAndSell(BuyExchange,SellExchange)
                 moveFunds(BuyExchange,SellExchange,BothExchanges)
-				
-				BuyExchange.tradingPaused = False
+                
+                BuyExchange.tradingPaused = False
                 SellExchange.tradingPaused = False
-				
-				with open('predicted_profits.csv','a') as c:
-					profitCalculations.to_csv(c,header=False)
+                
+                with open('predicted_profits.csv','a') as c:
+                    profitCalculations.to_csv(c,header=False)
             else:
                 time.sleep(0.5)
         
 
 def trial_arbitrage(BuyExchange,SellExchange,minProfitEuro):
 
-	BuyExchangeOrderbookThread = threading.Thread(target=BuyExchange.getOrderbooksContinuously,daemon=True)
-	SellExchangeOrderbookThread = threading.Thread(target=SellExchange.getOrderbooksContinuously,daemon=True)
-	BuyExchangeOrderbookThread.start()
-	SellExchangeOrderbookThread.start()
-	time.sleep(3)
+    BuyExchangeOrderbookThread = threading.Thread(target=BuyExchange.getOrderbooksContinuously,daemon=True)
+    SellExchangeOrderbookThread = threading.Thread(target=SellExchange.getOrderbooksContinuously,daemon=True)
+    BuyExchangeOrderbookThread.start()
+    SellExchangeOrderbookThread.start()
+    time.sleep(3)
 
-	while True:
-		profitCalculations = pc.profitabilityCalculator(BuyExchange,SellExchange)		
-		if profitCalculations['netProfitEuro'][0] > minProfitEuro:
-			
-			output_text = (
-							'Time:' + time.strftime("%d-%m-%Y %H:%M:%S") + '\n'
-							+ 'Predicted net profit (€): ' + profitCalculations['netProfitEuro'][0] + '\n'
-							+ 'Predicted net profit (BTC): ' + profitCalculations['netProfitBTC'][0] + '\n\n\n'
-							)
-			
-		with threading.Lock():
-			print(output_text)
-			with open('output_trial.txt','a') as t:
-				t.write(output_text)
-			with open('predicted_profits_trial.csv','a') as c:
-				profitCalculations.to_csv(c,header=False)		
+    while True:
+        profitCalculations = pc.profitabilityCalculator(BuyExchange,SellExchange)        
+        if profitCalculations['netProfitEuro'][0] > minProfitEuro:
             
-		time.sleep(5400)
-		else:
-			time.sleep(0.5)
+            output_text = (
+                            'Time:' + time.strftime("%d-%m-%Y %H:%M:%S") + '\n'
+                            + 'Predicted net profit (€): ' + profitCalculations['netProfitEuro'][0] + '\n'
+                            + 'Predicted net profit (BTC): ' + profitCalculations['netProfitBTC'][0] + '\n\n\n'
+                            )
+            
+            with threading.Lock():
+                print(output_text)
+                with open('output_trial.txt','a') as t:
+                    t.write(output_text)
+                with open('predicted_profits_trial.csv','a') as c:
+                    profitCalculations.to_csv(c,header=False)        
+                
+            time.sleep(5400)
+        else:
+            time.sleep(0.5)
 
-			
-			
+            
+            
 #========================================================================================================
 
 coins = {
-	'baseCurrency' : '_________',					#FILL IN BASE CURRENCY
-	'quoteCurrency' : '_________'					#FILL IN QUOTE CURRENCY
-		}
+    'baseCurrency' : '_________',             #FILL IN BASE CURRENCY
+    'quoteCurrency' : '_________'             #FILL IN QUOTE CURRENCY
+        }
 
 exchanges = {
-		'BUY':{
-			'exchangeName' : '_________',		#FILL IN BUY_EXCHANGE NAME
-			'publicKey' : '_________',			#FILL IN BUY_EXCHANGE PUBLIC API KEY
-			'privateKey' : '_________',			#FILL IN BUY_EXCHANGE PRIVATE API KEY
-			'withdrawalAddress' : '_________'	#FILL IN THE *SELL_EXCHANGE* DEPOSIT ADDRESS FOR THE BASE CURRENCY
-				},
-		'SELL':{
-			'exchangeName' : '_________', 		#FILL IN SELL_EXCHANGE NAME
-			'publicKey' : '_________', 			#FILL IN SELL_EXCHANGE PUBLIC API KEY
-			'privateKey' : '_________',			#FILL IN SELL_EXCHANGE PRIVATE API KEY
-			'withdrawalAddress' : '_________' 	#FILL IN THE *BUY_EXCHANGE* DEPOSIT ADDRESS FOR THE BASE CURRENCY
-				}
-			}
-			
+        'BUY':{
+            'exchangeName' : '_________',     #FILL IN BUY_EXCHANGE NAME
+            'publicKey' : '_________',        #FILL IN BUY_EXCHANGE PUBLIC API KEY
+            'privateKey' : '_________',       #FILL IN BUY_EXCHANGE PRIVATE API KEY
+            'withdrawalAddress' : '_________' #FILL IN THE *SELL_EXCHANGE* DEPOSIT ADDRESS FOR THE BASE CURRENCY
+                },
+        'SELL':{
+            'exchangeName' : '_________',     #FILL IN SELL_EXCHANGE NAME
+            'publicKey' : '_________',        #FILL IN SELL_EXCHANGE PUBLIC API KEY
+            'privateKey' : '_________',       #FILL IN SELL_EXCHANGE PRIVATE API KEY
+            'withdrawalAddress' : '_________' #FILL IN THE *BUY_EXCHANGE* DEPOSIT ADDRESS FOR THE BASE CURRENCY
+                }
+            }
+            
 minProfitEuro = 10
 trial = True
 
-			
+            
 if __name__ == '__main__':
-	
-	BuyExchange = Exchange(exchangeDict=exchanges['BUY'],exchangeType='BUY')
-	SellExchange = Exchange(exchangeDict=exchanges['SELL'],exchangeType='SELL')
-	BothExchanges = BothExchanges()
-	
-	if trial == True:
-		trial_arbitrage(BuyExchange,SellExchange,minprofitEuro)
-	else:
-		arbitrage(BuyExchange,SellExchange,bothExchanges,minProfitEuro)
+    
+    BuyExchange = Exchange(exchangeDict=exchanges['BUY'],exchangeType='BUY')
+    SellExchange = Exchange(exchangeDict=exchanges['SELL'],exchangeType='SELL')
+    BothExchanges = BothExchanges()
+    
+    if trial == True:
+        trial_arbitrage(BuyExchange,SellExchange,minProfitEuro)
+    else:
+        arbitrage(BuyExchange,SellExchange,BothExchanges,minProfitEuro)
 
